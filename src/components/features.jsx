2048cb7da3b98a871b7cb547854a8e76ebc7874e
Dhwaj Gupta <57860123+dhwaj1902@users.noreply.github.com>
2023-06-04 14:15:14 +0530

    indhan bazaar png logo added

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import indhanbazaar from "../img/IndhanBazaar.jpeg";
+import indhanbazaar from "../img/IndhanBazaar.png";
 
 export const Features = (props) => {
   return (
@@ -20,18 +20,35 @@ export const Features = (props) => {
               ))
             : "Loading..."}
         </div> */}
-                <img src={indhanbazaar} height={100} width={280}/>
+        <img src={indhanbazaar} height={80} width={300} />
         <div className="card-container">
-            <div className="float-layout">
-                <div className="card">
-                  <div className="card-title"><a style={{textDecoration: "none",color:"black"}} href="https://indhanbazaar.com/">Indhan Bazaar ™️</a></div>
-                  <div className="card-desc">
-                  <a style={{textDecoration: "none"}} href="https://indhanbazaar.com/">Indhan Bazaar</a> is an online marketplace platform for Biofuel products(briquette/pellets) known as green fuel. Here you can discover and connect with verified manufacturer, compare the biomass products as per your specific requirements and streamline your procurement by applying location based searches for on time deliveries.
-                  </div>
-                
+          <div className="float-layout">
+            <div className="card">
+              <div className="card-title">
+                <a
+                  style={{ textDecoration: "none", color: "black" }}
+                  href="https://indhanbazaar.com/"
+                >
+                  Indhan Bazaar ™️
+                </a>
+              </div>
+              <div className="card-desc">
+                <a
+                  style={{ textDecoration: "none" }}
+                  href="https://indhanbazaar.com/"
+                >
+                  Indhan Bazaar
+                </a>{" "}
+                is an online marketplace platform for Biofuel
+                products(briquette/pellets) known as green fuel. Here you can
+                discover and connect with verified manufacturer, compare the
+                biomass products as per your specific requirements and
+                streamline your procurement by applying location based searches
+                for on time deliveries.
               </div>
             </div>
           </div>
+        </div>
       </div>
     </div>
   );
